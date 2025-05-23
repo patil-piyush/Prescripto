de515675d1b829a957aa9835c66f02768a234a44
Piyush <piyushpatil0031@gmail.com>
2025-05-23 20:49:08 +0530

    initiated pages and navbar component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Home from './pages/Home.jsx'
+import Doctors from './pages/Doctors.jsx'
+import Login from './pages/Login.jsx'
+import Contact from './pages/Contact.jsx'
+import About from './pages/About.jsx'
+import MyAppointments from './pages/MyAppointments.jsx'
+import Appointments from './pages/Appointments.jsx'
+import MyProfile from './pages/MyProfile.jsx'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import './index.css';
+
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/doctors' element={<Doctors/>} />
+        <Route exact path='/doctors/:speciality' element={<Doctors/>} />
+        <Route exact path='/login' element={<Login/>} />
+        <Route exact path='/about' element={<About/>} />
+        <Route exact path='/contact' element={<Contact/> } />
+        <Route exact path='/my-profile' element={<MyProfile/> } />
+        <Route exact path='/my-appointments' element={<MyAppointments/> } />
+        <Route exact path='/appointments/:docId' element={<Appointments/> } />
+      </Routes>      
+      
+    </div>
+  )
+}
+
+export default App
