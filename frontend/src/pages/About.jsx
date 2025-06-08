081d8f35300e96dc879f09840c972082e707c9d8
Piyush <piyushpatil0031@gmail.com>
2025-06-08 23:22:48 +0530

    contact page done

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
       </div>
 
-      <div className='my-10 flex flex-col md:flex-row gap-12'>
+      <div className='my-10 flex flex-col md:flex-row gap-12 justify-center'>
         <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
           <p>Welcome to Prescripto, your trusted partner in managing your healthcare needs conveniently and efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
@@ -23,7 +23,7 @@ const About = () => {
         <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
       </div>
 
-      <div className='flex flex-col md:flex-row mb-20'>
+      <div className='flex flex-col md:flex-row mb-20 mt-5'>
         <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
           <b>Efficiency:</b>
           <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
