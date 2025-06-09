0cb36c817f9e6cd4abd928abe7797fd36449c598
Piyush <piyushpatil0031@gmail.com>
2025-06-09 19:09:22 +0530

    my-profile page done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { NavLink, useNavigate } from 'react-router-dom'
 const Navbar = () => {
     const navigate = useNavigate()
     const [showMenu,setShowMenu] = useState(false);
-    const [token, setToken] = useState(false);
+    const [token, setToken] = useState(true);
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-500'> 
         <img onClick={() => {navigate('/')}} className='w-44 cursor-pointer' src={assets.logo} alt="" />
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
           <p className='text-gray-500'>Tel: (415) 555‑0132 <br />Email: greatstackdev@gmail.com</p>
           <p className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
-          <button className='border border-black px-8 py-4 text-sm hover:bg-black text-white transition-all duration-500'>Explore Jobs</button>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
 
       </div>
